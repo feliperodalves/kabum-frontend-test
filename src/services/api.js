0cd24fe0d7bebe2e0bae519541066c5fb398fca9
Felipe Alves <felipe.rod.alves@gmail.com>
2020-01-05 20:16:28 -0300

    fix: https for axios mockapi.io

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://5e0f365e9576aa0014666493.mockapi.io',
+  baseURL: `https://5e0f365e9576aa0014666493.mockapi.io`,
 });
 
 export default api;
